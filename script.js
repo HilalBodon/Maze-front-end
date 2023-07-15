07345a188d567384e616c1347a6e42af9a38c982
HilalBodon <hilal_bodon@hotmail.com>
2023-07-15 19:17:38 +0300

    cleaning the code

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let bird2 = document.getElementById('bird2');
 let btn  = document.getElementById('btn');
 let rocks = document.getElementById('rocks');
 let forest = document.getElementById('forest');
-let header = document.getElementById('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -16,8 +15,6 @@ window.addEventListener('scroll', function(){
     btn.style.marginTop = value * 1.5 + 'px'; 
     rocks.style.top = value * -0.12 + 'px'; 
     forest.style.top = value * 0.25 + 'px'; 
-    header.style.top = value * 0.5 + 'px'; 
-
 });
 
 
