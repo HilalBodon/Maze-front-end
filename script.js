fa6e9309b97c5840d754096b1917973e05c9bc16
HilalBodon <hilal_bodon@hotmail.com>
2023-07-15 18:37:03 +0300

    changing images name

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let bird2 = document.getElementById('bird2');
 let btn  = document.getElementById('btn');
 let rocks = document.getElementById('rocks');
 let forest = document.getElementById('forest');
-let water = document.getElementById('water');
 let header = document.getElementById('header');
 
 window.addEventListener('scroll', function(){
